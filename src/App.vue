75a05809ee9014161a606014d8108ea437dd72ea
Travis Ricks <travis@defide-ix.com>
2021-09-09 17:21:21 +0900

    add scss support

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 @import '~bootstrap/dist/css/bootstrap.css';
 
 #app {
--- src/components/PremiumComponent.vue
+++ src/components/PremiumComponent.vue
@@ -45,5 +45,5 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 </style>
